1e8ddf78a3e05f4dbccdb72cea679017543a1b26
Alfredo Inostroza <jadenguy@gmail.com>
2019-09-26 15:48:35 -0400

    fixed oopsie where makeList would run forever since you're adding to the array, you need to stop at it's original length not the length after addition, since k and length grow by one each cycle

--- sketch.js
+++ sketch.js
@@ -8,14 +8,12 @@ let yOff = 0;
 function setup() {
   const list = makeList(10);
   const a = createVector(10);
-  // a.rotate(HALF_PI);
   b = createBinaryNode(list, a, TWO_PI);
   createCanvas(1600, 900);
   stroke(0);
   fill(220);
   xOff = 0;
   yOff = 0;
-  // noLoop();
 }
 
 function draw() {
@@ -23,10 +21,6 @@ function draw() {
   const o = createVector(xOff, yOff);
   const c = createVector(width / 2, height / 2);
   if (mDelta != 0) {
-    // const m = createVector(mouseX, mouseY);
-    // m.sub(c);
-    // m.mult(zoom);
-    // o.sub(m);
     m += mDelta;
   }
   const zoom = Math.pow(2, m);
@@ -36,7 +30,6 @@ function draw() {
   }
   o.mult(zoom);
   c.sub(o);
-  // print("drawing");
   b.Draw(c, zoom);
   mDelta = 0;
 }
@@ -47,12 +40,7 @@ function clamp(num, min, max) {
 function mouseWheel(event) {
   mDelta = -event.delta / 1000;
   return false;
-  // redraw();
 }
-// function mousePressed() {
-//   // x += 1;
-//   redraw();
-// }
 function makeList(len = 0) {
   const ret = [];
   for (let i = 0; i < len; i++) {
@@ -62,7 +50,8 @@ function makeList(len = 0) {
     ret.push(level);
     for (let j = 0; j < i; j++) {
       const lastLevel = ret[j];
-      for (let k = 0; k < lastLevel.length; k++) {
+      const lll = lastLevel.length;
+      for (let k = 0; k < lll; k++) {
         const current = lastLevel[k];
         lastLevel.push(current + root);
       }
